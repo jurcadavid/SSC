bd12f1dedc334f2a8ebe3f040515fbce3a0f0eb7
jurcadavid <91570583+jurcadavid@users.noreply.github.com>
2024-12-21 20:10:20 +0200

    Add files via upload

---
+++ adrese.c
@@ -0,0 +1,19 @@
+0x7ffff7c58740 p system
+0x7ffff7c47b90 p exit
+ffffe286   adresa shell
+
+0x7fffffffdd60  rbp
+0x7fffffffdd54  adresa de inceput de buffer
+0x7fffffffdd64  adresa de returnare
+
+X=16
+Y=20
+Z=24
+
+
+fara exit() ne va da segmentation fault
+
+
+
+
+
